24a43c0dc89878891dbce04056bf97af6332e173
YusufErdemK <yusuferdemyek@gmail.com>
2026-06-01 16:09:58 +0300

    feature: i started to write github registration function
    Signed-off-by: YusufErdemK <yusuferdemyek@gmail.com>

--- plotonix.py
+++ plotonix.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import os
 import sys
+from flask import json
 
 TEMPLATES_DIR = "/usr/local/share/plotonix/templates"
+GITHUBCRED_PATH = "json/github.json"
 
 TEMPLATE_MAP = {
     "classic": "classic-repo.py",
@@ -10,6 +12,17 @@ TEMPLATE_MAP = {
     "python": "python.py",
     "tool": "tool-repo.py"
 }
+
+def registerGitHub():
+    username = input("Enter your GitHub username: ")
+    token = input("Enter your GitHub personal access token: ")
+    
+    with open("json/github.json", "w") as f:
+        json.dump({"username": username, "token": token}, f)
+    
+    print("GitHub credentials registered successfully.")
+    print(f"Username: {username}")
+    
 
 def create_repo(template_key="classic"):
     template_file = TEMPLATE_MAP.get(template_key, "classic-repo.py")
@@ -31,6 +44,16 @@ def create_repo(template_key="classic"):
         print(f"Template '{template_file}' does not define create_structure().")
 
 def main():
+    try:
+        with open("json/github.json", "r") as neckhurt:
+            content = neckhurt.read()
+            if content:
+                pass
+            else:
+                registerGitHub()
+    except FileNotFoundError:
+        print(f"Error: {GITHUBCRED_PATH} not found.")
+    
     args = sys.argv[1:]
     template = "classic"
     if args:
